fb3859c6173168b6f1657d554d0d93fe9c6639db
ana <ana@anagram.shiftout.net>
2018-03-31 14:44:24 +0100

    Adds mjepg-streamer-rpi

---
+++ mjpeg-streamer-rpi/stream_from_pi.sh
@@ -0,0 +1,6 @@
+raspistill --nopreview -vf -hf -w 640 -h 480 -q 5 -o /tmp/stream/pic.jpg -tl 100 -t 9999999 -th 0:0:0 &
+
+until LD_LIBRARY_PATH=/usr/local/lib mjpg_streamer -i "input_file.so -f /tmp/stream -n pic.jpg" -o "output_http.so -w /usr/local/www"; do
+	echo "mjpg-streamer crashed with exit code $?.  Respawning..
+	sleep 1
+done
